8bb8ad4cbb19a3df70b845b613d378ff6fa4365c
eugeneshumilin <shumilinea@gmail.com>
2019-07-09 00:12:30 +0300

    add acceptance tests for sign in, sign up and sign out

---
+++ spec/features/devise/sign_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'authenticated user can sign out', %q{
+  In order to log out
+  as authenticated user
+  i want to be able to sign out
+} do
+  given(:user) { create(:user) }
+
+  scenario 'authenticated user tries sign out' do
+    sign_in(user)
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
