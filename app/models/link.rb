b98e28174fd143a80b1e84e8269a04e4d6e1d21e
eugeneshumilin <shumilinea@gmail.com>
2019-07-23 23:11:22 +0300

    add url validation for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
+  URL_FORMAT = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
+
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates :url, format: { with: URL_FORMAT, message:'please enter correct link' }
 end
